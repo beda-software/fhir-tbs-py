bf881ee3168d4386d0bcc700c8cb2ca3725588de
Vadim Laletin <rusmiligamer@gmail.com>
2024-09-30 19:28:23 +0200

    Add py.typed marker

--- aidbox_python_sdk_tbs/__init__.py
+++ aidbox_python_sdk_tbs/__init__.py
@@ -2,7 +2,7 @@ from .implementation import tbs_ctx_factory
 from .types import FilterBy, SubscriptionDefinition
 
 __title__ = "aidbox-python-sdk-tbs"
-__version__ = "0.0.1a4"
+__version__ = "0.0.1a5"
 __author__ = "beda.software"
 __license__ = "MIT"
 __copyright__ = "Copyright 2024 beda.software"
